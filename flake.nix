997d9f15b2938d784843c99a655cc207e037565c
sandorex <rzhw3h@gmail.com>
2026-04-19 20:38:33 +0200

    Merge nix devshells into one

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
         buildTools = "34.0.0";
         sdk = "35";
         ndk = "28.2.13676358";
-        cmake = "3.22.1";
       };
 
       androidComposition = pkgs.androidenv.composeAndroidPackages {
@@ -30,88 +29,74 @@
         platformVersions = [ androidVersion.sdk ];
         ndkVersions = [ androidVersion.ndk ];
         includeNDK = true;
-        cmakeVersions = [ androidVersion.cmake ];
       };
 
       rustToolchain = pkgs.rust-bin.stable.latest.default.override {
         targets = [
+          "x86_64-unknown-linux-gnu"
           "aarch64-linux-android"
-          # "armv7-linux-androideabi"
-          # "x86_64-linux-android"
+          "armv7-linux-androideabi"
+          # "x86_64-linux-android" # emulator
         ];
       };
+
+      sdlPkgs = with pkgs; [
+        pkg-config
+        just
+        cmake
+        validatePkgConfig
+        gcc
+        wayland-scanner
+        zenity
+        libffi
+        python313
+        patchelf
+        vulkan-headers
+        vulkan-loader
+        libGL
+        libusb1
+        libayatana-appindicator
+        libdrm
+        mesa
+        wayland
+        wayland-protocols
+        pipewire
+        libpulseaudio
+        alsa-lib
+        dbus
+        libxtst
+        libxcb
+        libxkbcommon
+        libx11
+        libxscrnsaver
+        libxcursor
+        libxext
+        libxfixes
+        libxi
+        libxrandr
+      ];
     in
     {
-      devShells.${system} = rec {
-        default = desktop;
-
-        desktop = pkgs.mkShell rec {
+      devShells.${system} = {
+        default = pkgs.mkShell rec {
           nativeBuildInputs = with pkgs; [
-            git
-
-            cargo
-            rust-analyzer
-            clippy
-            rustfmt
-            rustc
-
-            # basically SDL3 dependencies
-            libxtst
-            libxcb
-            just
-            cmake
-            validatePkgConfig
-            gcc
-            wayland-scanner
-            zenity
-            libffi
-            python313
-            patchelf
-            vulkan-headers
-            vulkan-loader
-            libGL
-            libusb1
-            libayatana-appindicator
-            libdrm
-            mesa
-            wayland
-            wayland-protocols
-            pipewire
-            libpulseaudio
-            alsa-lib
-            dbus
-            libxkbcommon
-            xorg.libX11
-            xorg.libXScrnSaver
-            xorg.libXcursor
-            xorg.libXext
-            xorg.libXfixes
-            xorg.libXi
-            xorg.libXrandr
-          ];
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeBuildInputs;
-        };
-
-        android = pkgs.mkShell rec {
-          inputsFrom = [ desktop ];
-
-          nativeBuildInputs = with pkgs; [
-            androidComposition.androidsdk
-            androidComposition.platform-tools
-            androidComposition.cmake
-            javaPackages.compiler.openjdk17
-
-            # rust
             rustToolchain
             cargo-ndk
-          ];
+
+            # android
+            androidComposition.androidsdk
+            androidComposition.platform-tools
+            javaPackages.compiler.openjdk17
+          ] ++ sdlPkgs;
+
+          # NOTE SDL3 cannot find video device, x11 or wayland without this
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath sdlPkgs;
 
           ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
           ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
           ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
+
           GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${androidVersion.buildTools}/aapt2";
-          CARGO_BUILD_TARGET = "aarch64-linux-android";
         };
       };
     };
