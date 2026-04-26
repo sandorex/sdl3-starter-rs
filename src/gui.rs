d21c36b6cd7a59c213d5d086cf8e9924ecd4946b
sandorex <rzhw3h@gmail.com>
2026-04-26 21:24:42 +0200

    Cleanup and tests for Rect
    This is still pretty much a skeleton

--- src/gui.rs
+++ src/gui.rs
@@ -2,13 +2,12 @@ use sdl3_sys::render::SDL_Renderer;
 
 pub use sdl3_sys::rect::{SDL_FRect, SDL_Rect};
 
-// TODO implement minus operator
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Amount {
     /// Percentage as a float, 50.0 is equal to 50%
     Percentage(f32),
 
-    /// Fraction as in 1/x
+    /// Fraction
     Fraction(f32),
 
     /// Size in pixels
@@ -16,57 +15,35 @@ pub enum Amount {
 }
 
 impl Amount {
-    fn calculate(&self, value: f32) -> (f32, f32) {
-        let (neg, x) = match self {
-            Self::Percentage(x) => {
-                let y = value * (x.abs() / 100.0);
-
-                (x.is_sign_negative(), y)
-
-                // // flip the output if the input is negative
-                // if x.is_sign_negative() {
-                //     (value - y, y)
-                // } else {
-                //     (y, value - y)
-                // }
-            },
-            Self::Fraction(x) => {
-                let y = value * (100.0 / x.abs() / 100.0);
-
-                (x.is_sign_negative(), y)
-                // if x.is_sign_negative() {
-                //     (value - y, y)
-                // } else {
-                //     (value - y, y)
-                // }
-            },
-            Self::Pixel(x) => {
-                // let neg = x.is_sign_negative();
-                // let x = x.abs();
-
-                (x.is_sign_negative(), x.abs())
-                // // flip the output if the input is negative
-                // if neg {
-                //     (value - x, x)
-                // } else {
-                //     (x, value - x)
-                // }
-            }
-        };
-
-        if neg {
-            (value - x, x)
-        } else {
-            (x, value - x)
+    fn negative(&self) -> bool {
+        match self {
+            Self::Percentage(x) => x.is_sign_negative(),
+            Self::Fraction(x) => x.is_sign_negative(),
+            Self::Pixel(x) => x.is_sign_negative(),
         }
     }
-    //
-    // pub fn abs(self) -> Self {
-    //     match self {
-    //         Self::Percentage(x) => Self::Percentage(x.abs()),
-    //         Self::Pixel(x) => Self::Pixel(x.abs()),
-    //     }
-    // }
+
+    fn calculate(&self, value: f32) -> (f32, f32) {
+        let x = match self {
+            Self::Percentage(x) => value * (x.abs() / 100.0),
+            Self::Fraction(x) => value * x.abs(),
+            Self::Pixel(x) => x.abs(),
+        };
+
+        (x, value - x)
+    }
+}
+
+impl std::ops::Neg for Amount {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            Self::Percentage(x) => Self::Percentage(-x),
+            Self::Fraction(x) => Self::Fraction(-x),
+            Self::Pixel(x) => Self::Pixel(-x),
+        }
+    }
 }
 
 impl From<f32> for Amount {
@@ -102,67 +79,82 @@ impl Rect {
         }
     }
 
-    // TODO rename to vert, horiz ?
-    /// Reserves certain amount vertically
+    /// Splits rect vertically, returning two rects
     pub fn vertical(self, amount: Amount) -> (Self, Self) {
         let (height, rem_height) = amount.calculate(self.height);
 
-        (
-            Self {
-                x: self.x,
-                y: self.y,
-                width: self.width,
-                height,
-            },
-            Self {
-                x: self.x,
-                y: self.y + height,
-                width: self.width,
-                height: rem_height,
-            }
-        )
+        if !amount.negative() {
+            (
+                Self {
+                    x: self.x,
+                    y: self.y,
+                    width: self.width,
+                    height,
+                },
+                Self {
+                    x: self.x,
+                    y: self.y + height,
+                    width: self.width,
+                    height: rem_height,
+                }
+            )
+        } else {
+            (
+                Self {
+                    x: self.x,
+                    y: self.y + rem_height,
+                    width: self.width,
+                    height: height,
+                },
+                Self {
+                    x: self.x,
+                    y: self.y,
+                    width: self.width,
+                    height: rem_height,
+                }
+            )
+        }
     }
 
     /// Reserves certain amount horizontally
     pub fn horizontal(self, amount: Amount) -> (Self, Self) {
         let (width, rem_width) = amount.calculate(self.width);
 
-        (
-            Self {
-                x: self.x,
-                y: self.y,
-                width,
-                height: self.height,
-            },
-            Self {
-                x: self.x + width,
-                y: self.y,
-                width: rem_width,
-                height: self.height,
-            }
-        )
+        if !amount.negative() {
+            (
+                Self {
+                    x: self.x,
+                    y: self.y,
+                    width,
+                    height: self.height,
+                },
+                Self {
+                    x: self.x + width,
+                    y: self.y,
+                    width: rem_width,
+                    height: self.height,
+                }
+            )
+        } else {
+            (
+                Self {
+                    x: self.x + rem_width,
+                    y: self.y,
+                    width: width,
+                    height: self.height,
+                },
+                Self {
+                    x: self.x,
+                    y: self.y,
+                    width: rem_width,
+                    height: self.height,
+                }
+            )
+        }
     }
-
-    /// Divide vertically in two
-    pub fn halve_v(self) -> (Self, Self) { self.vertical(50.0.into()) }
-
-    /// Divide horizontally in two
-    pub fn halve_h(self) -> (Self, Self) { self.horizontal(50.0.into()) }
-
-    // TODO remove once the Amount::Fraction is done
-    /// Divide vertically by a fraction 1/x
-    pub fn fraction_v(self, div: f32) -> (Self, Self) { self.vertical((100.0 / div).into()) }
-
-    /// Divide horizontally by a fraction 1/x
-    pub fn fraction_h(self, div: f32) -> (Self, Self) { self.horizontal((100.0 / div).into()) }
 
     /// Add margin to the rect but each margin can be different
     pub fn margin_all(self, up: f32, down: f32, left: f32, right: f32) -> Self {
-        let up = up.abs();
-        let down = down.abs();
-        let left = left.abs();
-        let right = right.abs();
-
         Self {
             x: self.x + left,
             y: self.y + up,
@@ -178,12 +170,6 @@ impl Rect {
 
         self.margin_all(up, up, left, left)
     }
-
-    /// Add margin to top and bottom
-    pub fn margin_v(self, margin: f32) -> Self { self.margin_all(margin, margin, 0.0, 0.0) }
-
-    /// Add margin to left and right
-    pub fn margin_h(self, margin: f32) -> Self { self.margin_all(0.0, 0.0, margin, margin) }
 
     /// Center vertically
     pub fn center_v(self, height: f32) -> Option<Self> {
@@ -256,7 +242,6 @@ mod tests {
     use std::ops::Deref;
     use super::{Rect, Amount};
 
-    /// Wrapper around rect that can be compared without rounding errors
     #[derive(Debug)]
     struct RectWrapper(Rect);
 
@@ -311,40 +296,20 @@ mod tests {
     }
 
     #[test]
+    fn test_amount() {
+        assert_eq!(Amount::Percentage(30.).calculate(300.), (90., 210.));
+        assert_eq!(Amount::Fraction(1./3.).calculate(300.), (100., 200.));
+        assert_eq!(Amount::Pixel(100.).calculate(300.), (100., 200.));
+
+        // negative amounts (they should be the same)
+        assert_eq!(Amount::Percentage(-30.).calculate(300.), (90., 210.));
+        assert_eq!(Amount::Fraction(-1./3.).calculate(300.), (100., 200.));
+        assert_eq!(Amount::Pixel(-100.).calculate(300.), (100., 200.));
+    }
+
+    #[test]
     fn test_rect() {
-        rect_tuple_eq!(
-            Rect::new(0.0, 0.0, 200.0, 100.0).halve_v(),
-            (
-                Rect::new(0.0, 0.0, 200.0, 50.0),
-                Rect::new(0.0, 50.0, 200.0, 50.0),
-            )
-        );
-
-        rect_tuple_eq!(
-            Rect::new(0.0, 0.0, 200.0, 100.0).halve_h(),
-            (
-                Rect::new(0.0, 0.0, 100.0, 100.0),
-                Rect::new(100.0, 00.0, 100.0, 100.0),
-            )
-        );
-
-        rect_tuple_eq!(
-            Rect::new(0.0, 0.0, 200.0, 100.0).fraction_v(3.0),
-            (
-                Rect::new(0.0, 0.0, 200.0, 33.3333),
-                Rect::new(0.0, 33.3333, 200.0, 66.6666),
-            )
-        );
-
-        rect_tuple_eq!(
-            Rect::new(0.0, 0.0, 200.0, 100.0).fraction_v(-3.0),
-            (
-                Rect::new(0.0, 0.0, 200.0, 66.6666),
-                Rect::new(0.0, 66.6666, 200.0, 33.33333),
-            )
-        );
-
-        // test reserving with pixels (both directions)
+        // vertical
         rect_tuple_eq!(
             Rect::new(0.0, 0.0, 200.0, 100.0).vertical(Amount::Pixel(30.0)),
             (
@@ -356,12 +321,12 @@ mod tests {
         rect_tuple_eq!(
             Rect::new(0.0, 0.0, 200.0, 100.0).vertical(Amount::Pixel(-30.0)),
             (
-                Rect::new(0.0, 0.0, 200.0, 70.0),
                 Rect::new(0.0, 70.0, 200.0, 30.0),
+                Rect::new(0.0, 0.0, 200.0, 70.0),
             )
         );
 
-        // now horizontal
+        // horizontal
         rect_tuple_eq!(
             Rect::new(0.0, 0.0, 200.0, 100.0).horizontal(Amount::Pixel(50.0)),
             (
@@ -373,26 +338,57 @@ mod tests {
         rect_tuple_eq!(
             Rect::new(0.0, 0.0, 200.0, 100.0).horizontal(Amount::Pixel(-50.0)),
             (
-                Rect::new(0.0, 0.0, 150.0, 100.0),
                 Rect::new(150.0, 0.0, 50.0, 100.0),
+                Rect::new(0.0, 0.0, 150.0, 100.0),
             )
         );
 
-        // TODO
+        // vertical fraction
+        rect_tuple_eq!(
+            Rect::new(0.0, 0.0, 200.0, 100.0).vertical(Amount::Fraction(1./3.)),
+            (
+                Rect::new(0.0, 0.0, 200.0, 33.3333),
+                Rect::new(0.0, 33.3333, 200.0, 66.6666),
+            )
+        );
+
+        rect_tuple_eq!(
+            Rect::new(0.0, 0.0, 200.0, 100.0).vertical(Amount::Fraction(-1./3.)),
+            (
+                Rect::new(0.0, 66.6666, 200.0, 33.3333),
+                Rect::new(0.0, 0.0, 200.0, 66.6666),
+            )
+        );
+
+        // margin
+        rect_eq!(
+            Rect::new(0.0, 0.0, 200.0, 100.0).margin(Amount::Pixel(5.)),
+            Rect::new(5., 5., 190., 90.)
+        );
+
+        // margin all
+        rect_eq!(
+            Rect::new(0.0, 0.0, 200.0, 100.0).margin_all(2., 4., 6., 8.),
+            Rect::new(6., 2., 186., 94.)
+        );
+
+        // center
+        rect_eq!(
+            Rect::new(0.0, 0.0, 200.0, 100.0).centered(100., 50.).unwrap(),
+            Rect::new(50., 25., 100., 50.)
+        );
+    }
+
+    #[test]
+    fn test_rect_complex() {
+        // a basic layout
         let rect = Rect::new(0., 0., 800., 600.);
         let (toolbar, rect) = rect.vertical(Amount::Pixel(30.));
-        // dbg!(&rect);
-        let (sidebar, rect) = rect.horizontal(Amount::Pixel(60.));
-        dbg!(&rect);
-        // let content = rect.margin(Amount::Pixel(10.));
-//  p{ x: 0.0, y: 30.0, width: 740.0, height: 570.0 })
-        rect_eq!(toolbar, Rect::new(0., 30., 740., 570.));
-        rect_eq!(sidebar, Rect::new(0., 0., 0., 0.));
-        // rect_eq!(content, Rect::new(0., 0., 0., 0.));
-        //
-        // // TODO i cannot add sidebar to the right side?
-        // assert_eq!(toolbar, Rect::new(0., 0., 800., 30.));
-        // assert_eq!(sidebar, Rect::new(770.0, 0., 60., 570.));
-        // assert_eq!(content, Rect::new(0., 0., 60., 570.));
+        let (sidebar, rect) = rect.horizontal(Amount::Pixel(-60.));
+        let content = rect.margin(Amount::Pixel(10.));
+
+        rect_eq!(toolbar, Rect::new(0., 0., 800., 30.));
+        rect_eq!(sidebar, Rect::new(740., 30., 60., 570.));
+        rect_eq!(content, Rect::new(10., 40., 720., 550.));
     }
 }
